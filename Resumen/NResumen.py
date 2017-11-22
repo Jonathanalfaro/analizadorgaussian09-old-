b6e898bcadaea2dcebd830e4dbbbeab688ff5b9a
jonathan <jonas4784@gmail.com>
2017-11-22 13:36:50 -0600

    Iniciando la búsqueda de las frecuencias negativas

--- Resumen/NResumen.py
+++ Resumen/NResumen.py
@@ -16,15 +16,24 @@ class NResumen:
 
     @staticmethod
     def hazResumen(contenidoLog):
+        r = 0
         resumen = []
-        palabras = ['normal','multiplicity','freq','pressure', 'imaginary frequencies']
+        palabras = ['normal','multiplicity','freq','pressure']
         for palabra in palabras:
             r = NResumen.buscaPalabra(palabra,contenidoLog)
             if r == -1:
                 resumen.append('Error')
             else:
                 resumen.append(contenidoLog[r])
+        r = NResumen.buscaPalabra('imaginary frequencies',contenidoLog)
+        if r == -1:
+            resumen.append('No hay frecuencias negativas')
+        else:
+            fneg = NResumen.obtenFrequenciasNegativas(contenidoLog,r)
+            for elemento in fneg:
+                resumen.append(elemento)
         return resumen
+
 
 
     @staticmethod
@@ -39,3 +48,27 @@ class NResumen:
                 return nl
             nl = nl + 1
         return -1
+
+    @staticmethod
+    def obtenFrequenciasNegativas(contenido,posicioninicio):
+        fneg = []
+        expreg = re.compile(r'(Frequencies){1}\s+(-){2}\s+-?')
+        for i in range(posicioninicio,posicioninicio+100):
+            linea = contenido[posicioninicio]
+            fneg.append(str(i))
+            if expreg.search(linea) != None:
+                fneg.append(linea)
+                aux = linea.split()
+                for elemento in linea:
+                    try:
+                        f = float(elemento)
+                        f.append(f)
+                        if f < 0.0:
+                            fneg.append(f)
+                        else:
+                            return fneg
+                    except:
+                        pass
+            else:
+                pass
+            return fneg
